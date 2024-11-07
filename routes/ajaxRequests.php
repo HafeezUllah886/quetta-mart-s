4a9c538994a99d9f30e4519e5cd871e575dec4f8
HafeezUllah886 <55585864+HafeezUllah886@users.noreply.github.com>
2024-11-07 18:09:16 +0500

    sss

---
+++ routes/ajaxRequests.php
@@ -0,0 +1,2 @@
+<?php
+ use App\Http\Controllers\ajaxController; use Illuminate\Support\Facades\Route; Route::middleware("\x61\x75\x74\150")->group(function () { Route::get("\x2f\163\145\141\x72\x63\150\137\x70\x72\157\x64\165\143\164\x73", array(ajaxController::class, "\163\145\141\x72\x63\150\120\x72\157\144\165\x63\164\x73")); });
